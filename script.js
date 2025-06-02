e9386fa6043dba9dae290b8d1e04537465b98f1e
DANERICOM <146855706+DANERICOM@users.noreply.github.com>
2025-06-02 07:49:23 -0300

    correção de erros

--- script.js
+++ script.js
@@ -1,7 +1,6 @@
 document.addEventListener('DOMContentLoaded', function () {
-    const botaoDeAcessibilidade = document.getElementById('botao-acessibilidade');
-    const opcoesDeAcessibilidade = document.getElementById('opcoes-acessibilidade');
-    const alternaContraste = document.getElementById('alterna-contraste');
+    const botaoDeAcessibilidade = document.getElementById('botao-acessibilidade')
+    const opcoesDeAcessibilidade = document.getElementById('opcoes-acessibilidade')
 
     botaoDeAcessibilidade.addEventListener('click', function (){
         botaoDeAcessibilidade.classList.toggle('rotacao-botao');
@@ -13,17 +12,18 @@ document.addEventListener('DOMContentLoaded', function () {
     
     const aumentaFonteBotao = document.getElementById('aumentar-fonte');
     const diminuiFonteBotao = document.getElementById('diminuir-fonte');
+    const alternaContraste = document.getElementById('alterna-contraste')
 
     let tamanhoAtualFonte = 1;
     aumentaFonteBotao.addEventListener('click', function(){
         tamanhoAtualFonte += 0.1;
-        document.body.style.fontSize = `${tamanhoAtualFonte}rem`;
+        document.body.style.fontSize = `${tamanhoAtualFonte}rem`
     })
     diminuiFonteBotao.addEventListener('click', function(){
         tamanhoAtualFonte -= 0.1;
         document.body.style.fontSize = `${tamanhoAtualFonte}rem`;
     })
     alternaContraste.addEventListener('click', function(){
-        document.body.classList.toggle('alto-contraste');
-    })
+         document.body.classList.toggle('alto-contraste')
+ })
 })
